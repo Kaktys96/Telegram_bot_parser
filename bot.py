ce9eeb56b12c0660cbd0eba56e2a002f1a5b031d
Kaktys96 <Kaktys96@tut.by>
2025-05-04 00:30:51 +0300

    Bot finished

--- bot.py
+++ bot.py
@@ -2,13 +2,17 @@ import time
 import telebot
 import threading
 from flashbot2 import enter_site, check_score
+from dotenv import load_dotenv
+import os
 
-TELEGRAM_TOKEN = "6308284018:AAFcvYUPwN8b2LUYJfmgmQbW5e-0tVPag_s"
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 channel = '@flashscore_bot_my'
 sent_urls_file = 'sent_urls.txt'
 
-# Загружаем уже отправленные ссылки из файла
+
 def load_sent_urls():
     try:
         with open(sent_urls_file, 'r') as f:
@@ -16,12 +20,14 @@ def load_sent_urls():
     except FileNotFoundError:
         return set()
 
-# Сохраняем новые ссылки
+
 def save_sent_url(url):
     with open(sent_urls_file, 'a') as f:
         f.write(url + '\n')
 
+
 all_send_url = load_sent_urls()
+
 
 def run():
     while True:
@@ -50,7 +56,7 @@ def run():
         except Exception as exc:
             print("Ошибка в основном цикле:", exc)
 
-        time.sleep(60)  # Пауза перед следующим запуском
+        time.sleep(60)
 
 
 if __name__ == '__main__':
